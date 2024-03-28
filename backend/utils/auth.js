60b352dc84a4a03bac0a7d9bcaadbae205cbdc4e
Alan <19deleon88@gmail.com>
2024-03-28 09:51:23 +0900

    finished auth middleware

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -3,6 +3,63 @@ const {jwtConfig} = require('../config');
 const {User} = require('../db/models');
 const {secret, expiresIn} = jwtConfig;
 
-const setTokenCookie = (req, user) => {
+const setTokenCookie = (res, user) => {
+    // create token cookie
+    const safeUser = user.toSafeObject();
+
+    const token = jwt.sign(
+        {data: safeUser},
+        secret,
+        {expiresIn: parseInt(expiresIn)}
+    );
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    res.cookie('token', token,{
+        maxAge: expiresIn * 1000,
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && 'Lax'
+    });
+
+    return token;
+}
+
+const restoreUser = (req, res, next) =>{
+    const { token } = req.cookies;
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async(err, jwtPayload) =>{
+        if(err){
+            return next()
+        }
+        try{
+            const { id } = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+        }
+        catch(e){
+            res.clearCookie('token');
+            return next();
+        }
+        if(!req.user) res.clearCookie('token');
+        return next();
+    })
+}
+
+const requireAuth = (req, _res, next) =>{
+    if(req.user) return next();
+
+    const err = new Error('Authentication required');
+    err.title = 'Authentication required';
+    err.errors = {
+        messge: 'Authentiction required'
+    };
+    err.status = 401;
+    return next(err);
 
 }
+
+module.exports = {
+    setTokenCookie,
+    restoreUser,
+    requireAuth
+};
