5d77e30a3d1910f12463f9176c810090cbf5f10a
Alan <19deleon88@gmail.com>
2024-03-28 12:06:58 +0900

    finished auth middleware and connected to api routes

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonebtoken');
+const jwt = require('jsonwebtoken');
 const {jwtConfig} = require('../config');
 const {User} = require('../db/models');
 const {secret, expiresIn} = jwtConfig;
