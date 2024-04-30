f61d74e1ed7c65ccac34b24f7989fcd06b4ba0fd
AlanDeleon88 <19deleon88@gmail.com>
2024-04-29 21:56:44 -0700

    finished testing util routes, finished setting up index api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,40 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth} = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-})
+router.use(restoreUser)
+
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+
+//     setTokenCookie(res, user);
+
+//     return res.json({ user: user });
+// })
+
+// router.get(
+//     '/restore-user',
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// )
+
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// )
+
+// router.post('/test', (req, res) => {
+//     res.json({ requestBody: req.body });
+// })
+
+
 
 module.exports = router;
