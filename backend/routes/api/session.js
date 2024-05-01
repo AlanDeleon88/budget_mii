e203815f09cfc7bd86bfbafe7ea5461274e8a027
AlanDeleon88 <19deleon88@gmail.com>
2024-04-30 21:46:33 -0700

    added logout endpoint to session router

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,28 +10,32 @@ const { User } = require('../../db/models');
 const router = express.Router();
 
 
-router.post('/', async (req, res, next) => {
-    // const { credential, password } = req.body;
+router.route('/')
+    .post(async (req, res, next) => {
+        // const { credential, password } = req.body;
 
-    // console.log(credential)
+        // console.log(credential)
 
-    const user = await User.login(req.body);
+        const user = await User.login(req.body);
 
-    if (!user) {
-        const loginErr = buildError('Login Failed', 'Login Failed', 401)
-        loginErr.errors = { credential: 'The provided credentials were invalid' }
-        return next(loginErr);
-    }
+        if (!user) {
+            const loginErr = buildError('Login Failed', 'Login Failed', 401)
+            loginErr.errors = { credential: 'The provided credentials were invalid' }
+            return next(loginErr);
+        }
 
-    const userObj = user.toSafeObject();
+        const userObj = user.toSafeObject();
 
-    await setTokenCookie(res, userObj);
+        await setTokenCookie(res, userObj);
 
-    return res.json({user: userObj})
+        return res.json({ user: userObj })
+    })
+    .delete(async (_req, res, next) => {
+        res.clearCookie('token');
+        return res.json({ message: 'log out successful' });
+    })
 
 
-
-})
 
 
 
