2f96c047b46034b2aa26b7c00db9cfd183ec8824
AlanDeleon88 <19deleon88@gmail.com>
2024-05-02 21:14:20 -0700

    added signup body validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,14 +1,36 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 
 const { User } = require('../../db/models');
 
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or mode.'),
+    handleValidationErrors
+]
+
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
+router.post('/', validateSignup, async (req, res, next) => {
     // const { username, email, password } = req.body;
 
     const newUser = await User.signup(req.body);
